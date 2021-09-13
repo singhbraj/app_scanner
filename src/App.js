f5c8ffbb1a18407f31ad6602d5f0f839844ce4c6
unknown <braj.singh@truemeds.in>
2021-09-13 13:55:44 +0530

    third-commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react'
-import QrReader from 'react-qr-reader'
+import QrReader from 'react-qr-scanner'
 
 class App extends Component {
-  state = {
-    result: 'No result'
-  }
-
-  handleScan = data => {
-    if (data) {
-      this.setState({
-        result: data
-      })
+  constructor(props){
+    super(props)
+    this.state = {
+      delay: 100,
+      result: 'No result',
     }
+
+    this.handleScan = this.handleScan.bind(this)
   }
-  handleError = err => {
+  handleScan(data){
+    this.setState({
+      result: data,
+    })
+  }
+  handleError(err){
     console.error(err)
   }
-  render() {
-    return (
+  render(){
+    const previewStyle = {
+      height: 240,
+      width: 320,
+    }
+
+    return(
       <div>
         <QrReader
-          delay={300}
+          delay={this.state.delay}
+          style={previewStyle}
           onError={this.handleError}
           onScan={this.handleScan}
-          style={{ width: '100%' }}
-        />
+          />
         <p>{this.state.result}</p>
       </div>
     )
   }
 }
+
 export default App

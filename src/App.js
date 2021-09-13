7e0efeb09742141678462588e1c04dcbe86bf82a
unknown <braj.singh@truemeds.in>
2021-09-13 14:37:25 +0530

    modal added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
-import React, { Component } from 'react'
-import QrReader from 'react-qr-scanner'
+import React, { Component } from "react";
+import QrReader from "react-qr-reader";
+import { Modal, Button } from "antd";
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      delay: 100,
-      result: 'No result',
-    }
+  state = {
+    result: "Braj",
+    openModal: false,
+  };
 
-    this.handleScan = this.handleScan.bind(this)
-  }
-  handleScan(data){
-    this.setState({
-      result: data,
-    })
-  }
-  handleError(err){
-    console.error(err)
-  }
-  render(){
-    const previewStyle = {
-      height: 240,
-      width: 320,
+  handleScan = (data) => {
+    if (data) {
+      this.setState({
+        result: data,
+      });
     }
-
-    return(
+  };
+  handleError = (err) => {
+    console.error(err);
+  };
+  render() {
+    return (
       <div>
-        <QrReader
-          delay={this.state.delay}
-          style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan}
+        <Button type="primary" 
+        onClick={()=>this.setState({ openModal: true })}>
+          Scancode
+        </Button>
+        <Modal
+          type="Scan the Qr Code"
+          visible={this.state.openModal}
+          onCancel={() => this.setState({ openModal: false })}
+          footer={null}
+        >
+          <QrReader
+            delay={300}
+            onError={this.handleError}
+            onScan={this.handleScan}
+            style={{ width: "100%" }}
           />
-        <p>{this.state.result}</p>
+          <p style={{color:'black'}}>{this.state.result}</p>
+        </Modal>
       </div>
-    )
+    );
   }
 }
-
-export default App
+export default App;
